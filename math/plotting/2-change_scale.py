45c2ea5420fa7a2dcbdc2296577229e0ad201cc7
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-12 21:40:26 +0200

    2

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -7,9 +7,10 @@ r = np.log(0.5)
 t = 5730
 y = np.exp((r / t) * x)
 
-# your code here
 plt.title('Exponential Decay of C-14')
 plt.plot(x, y)
 plt.yscale('log')
+plt.ylabel('Fraction Remaining')
 plt.xlim(0, 28651)
+plt.xlabel('Time (years)')
 plt.show()
