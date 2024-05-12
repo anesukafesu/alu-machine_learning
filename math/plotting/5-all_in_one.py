ec36854bf120ac4b05708c779d235c1146fa2d10
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-12 21:57:07 +0200

    swapped 5 and 6

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -2,32 +2,89 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Creating the data
+# Generate Data
+# Plot 1
+x0 = np.arange(0, 11)
+y0 = x0 ** 3
+
+# Plot 2
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+y1 += 180
 
-# Creating the labels and options
-names = ['Farrah', 'Fred', 'Felicia']
-colors = ['red', 'yellow', 'orange', '#ffe5b4']
-fruit_names = ['apples', 'bananas', 'oranges', 'peaches']
-x = [1, 2, 3]
+# Plot 3
+x2 = np.arange(0, 28651, 5730)
+r2 = np.log(0.5)
+t2 = 5730
+y2 = np.exp((r2 / t2) * x2)
 
-# Initialising bottoms array
-bottoms = np.zeros(fruit.shape[1])
+# Plot 4
+x3 = np.arange(0, 21000, 1000)
+r3 = np.log(0.5)
+t31 = 5730
+t32 = 1600
+y31 = np.exp((r3 / t31) * x3)
+y32 = np.exp((r3 / t32) * x3)
 
-# Plot title
-plt.title('Number of Fruit Per Person')
+# Plot 5
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
 
-# Plotting each fruit
-for i in range(fruit.shape[0]):
-  color = colors[i]
-  fruit_name = fruit_names[i]
-  height = fruit[i]
+# Create Grid
+fig = plt.figure(constrained_layout=True)
+gs = fig.add_gridspec(3, 2)
 
-  plt.bar(x, height=height, bottom=bottoms, color=color, width=0.5, tick_label=names, label=fruit_name)
-  bottoms += height
+# Create title
+fig.suptitle('All in One')
 
-plt.ylim(0, 80)
-plt.ylabel('Quantity of Fruit')
-plt.legend()
+# Create plots
+plot1 = fig.add_subplot(gs[0, 0])
+plot2 = fig.add_subplot(gs[0, 1])
+plot3 = fig.add_subplot(gs[1, 0])
+plot4 = fig.add_subplot(gs[1, 1])
+plot5 = fig.add_subplot(gs[2, :])
+
+# Title formatting
+fontsize = 6
+
+# Plot first graph
+plot1.plot(x0, y0, color='red')
+plot1.set_xlim((0, 10))
+
+# Plot the second graph
+plot2.scatter(x1, y1, color='magenta')
+plot2.set_title("Men's Height vs Weight", fontsize=fontsize)
+plot2.set_xlabel('Height (in)', fontsize=fontsize)
+plot2.set_ylabel('Weight (lbs)', fontsize=fontsize)
+
+# Plot the third graph
+plot3.set_title('Exponential Decay of C-14', fontsize=fontsize)
+plot3.set_xlabel('Time (years)', fontsize=fontsize)
+plot3.set_ylabel('Fraction Remaining', fontsize=fontsize)
+plot3.plot(x2, y2)
+plot3.set_yscale('log')
+plot3.set_xlim(0, 28651)
+
+# Plot the fourth graph
+plot4.set_title('Exponential Decay of Radioactive Elements', fontsize=fontsize)
+plot4.plot(x3, y31, color='red', linestyle='dashed', label='C-14')
+plot4.plot(x3, y32, color='green', label='Ra-226')
+plot4.set_xlim((0, 20000))
+plot4.set_ylim((0, 1))
+plot4.set_xlabel('Time (years)', fontsize=fontsize)
+plot4.set_ylabel('Fraction Remaining', fontsize=fontsize)
+plot4.legend(fontsize=fontsize)
+
+# Plot the fifth graph
+plot5.set_title('Project A', fontsize=fontsize)
+plot5.hist(student_grades, range=(0, 100), bins=10, edgecolor='black')
+plot5.set_ylabel('Number of Students', fontsize=fontsize)
+plot5.set_ylim([0, 30])
+plot5.set_xlabel('Grades', fontsize=fontsize)
+plot5.set_xlim([0, 100])
+x_ticks = np.arange(0, 101, 10)
+plot5.set_xticks(x_ticks)
+
 plt.show()
