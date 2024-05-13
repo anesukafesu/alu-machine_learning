533bdf81fe77bdf51bfea3c217f863525cea4b3f
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-13 21:36:54 +0200

    7

---
+++ math/linear_algebra/7-getting_cozy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        
+        return mat1.copy() + mat2.copy()
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        
+        result = []
+        
+        for row1, row2 in zip(mat1, mat2):
+            combined_row = row1 + row2
+            result.append(combined_row)
+        
+        return result
