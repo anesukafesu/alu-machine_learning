835dfcddf3b40b7b1c291dfa151586fa33ef96a7
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-13 20:18:36 +0200

    3

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 
-def 
+def matrix_shape(matrix):
+    current_dimension = matrix
+    shape = []
+
+    while isinstance(current_dimension, list):
+        shape.append(len(current_dimension))
+        current_dimension = current_dimension[0]
+
+    return shape
