3082e3d437db9cbb81212a5d9c88fda8432dd7dd
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-14 21:27:46 +0200

    pycodestyle

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -8,10 +8,10 @@ def add_arrays(arr1, arr2):
     """
     if len(arr1) != len(arr2):
         return None
-    
+
     result = []
 
     for num1, num2 in zip(arr1, arr2):
         result.append(num1 + num2)
-    
+
     return result
