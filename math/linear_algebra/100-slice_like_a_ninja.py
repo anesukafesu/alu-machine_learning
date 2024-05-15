8401233c1c080e2ad06af1dc1e76ed15eab09f7c
Anesu Kafesu <a.kafesu@alustudent.com>
2024-05-15 18:04:34 +0200

    100

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 """ Implements the np_slice method
 """
+import numpy as np
 
 def np_slice(matrix, axes={}):
     """ Slices a matrix along specified axes and limits
     """
-    zero = axes.get(0)
-    one = axes.get(1)
     result = matrix
 
-    if zero is not None:
-        # Extract the parameters for the slice
-        start, end, step = extract_slice_params(zero)
+    {}.items()
+
+    for axis, slice_params in axes.items():
+        # Extract the slice params
+        start, end, step = extract_slice_params(slice_params)
+
+        # Create indices to use to get data
+        indices = np.arange(start, end, step)
 
         # Get rows from start to end using step
-        result = result[start: end: step]
-        
-
-    if one is not None:
-        # Extract the parameters for the slice
-        start, end, step = extract_slice_params(one)
-
-        # Get columns from start to end using step
-        result = result[:, start: end: step]
+        result = np.take(result, indices, axis)
 
     return result
 
@@ -30,10 +26,15 @@ def np_slice(matrix, axes={}):
 def extract_slice_params(slice_tuple):
     start = end = step = None
 
+    # If only one value has been provided, it is the end value
     if len(slice_tuple) == 1:
         end, = slice_tuple
+
+    # If two values have been provided, then they are the start and end values
     elif len(slice_tuple) == 2:
         start, end = slice_tuple
+
+    # If three values have been provided, then they are the start, end and stop values
     elif len(slice_tuple) == 3:
         start, end, step = slice_tuple
     
