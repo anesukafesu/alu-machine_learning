13b6ea3e462c40e288bda6299ddc5883a1dae3d4
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-01 11:35:02 +0200

    fixes

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """ Implements the poly_derivative function
 """
-import numpy as np
 
 
 def poly_derivative(poly):
     """ Calculates the derivative of a given polynomial
     """
-    p = np.array(poly)
-    powers = np.arange(p.size)
-
-    return (p * powers)[1:]
+    return list(map(poly, lambda x, i: x * i)[1:])
