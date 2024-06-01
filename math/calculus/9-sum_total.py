7d1224945b6a85b51bba72f6a48a75b5410bd92c
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-01 11:46:29 +0200

    more fixes

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,4 +6,7 @@
 def summation_i_squared(n):
     """ Sums up the squares of all terms from 1 to n
     """
+    if not isinstance(n, int) or n <= 0:
+        return None
+
     return sum(map(lambda x: x ** 2, range(1, n + 1)))
