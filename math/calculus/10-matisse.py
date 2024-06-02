495f0c8b1e387f5d372ef546e0fa66697b63fe2c
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-02 08:02:06 +0200

    17

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -8,8 +8,28 @@ def poly_derivative(poly):
     """
     if not isinstance(poly, list):
         return None
-    
+
+    if not every(is_number, poly):
+        return None
+
     if len(poly) == 1:
         return [0]
-    
+
     return list(map(lambda x, i: x * i, poly, range(len(poly))))[1:]
+
+
+def is_number(x):
+    """ Returns True if x is either an int or float
+    """
+    return isinstance(x, int) or isinstance(x, float)
+
+
+def every(func, iter):
+    """ Returns True if each value in the iter returns True
+    after being passed to func. Else it returns False
+    """
+    for val in iter:
+        if not func(val):
+            return False
+
+    return True    
