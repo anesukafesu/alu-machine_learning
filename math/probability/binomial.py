3535dcdfa73e3091f9183a07dac96f95d69a06ce
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-09 20:17:54 +0200

    pycodestyle

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -27,14 +27,15 @@ class Binomial:
 
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            
+
             # Infer parameters from data
 
             # Calculate the sample mean = np
             sample_mean = sum(data) / len(data)
 
             # Calculate the sample variance = npq
-            sample_variance = sum(map(lambda x: x ** 2, data)) / len(data) - sample_mean ** 2
+            sample_variance = sum(map(lambda x: x ** 2, data))\
+                / len(data) - sample_mean ** 2
 
             # q = npq / np = sample_variance / sample_mean
             q = sample_variance / sample_mean
@@ -55,41 +56,47 @@ class Binomial:
     def pmf(self, k):
         """ Calculates the PMF of a given number of successes
         Args:
-            k (float | int): the number of successes whose pmf is to be determined
+            k (float | int): the number of successes whose
+            pmf is to be determined
         Returns:
             p (float): the probability of k successes
         """
         k = int(k)
-        
+
         if k < 0:
             return 0
 
-        return self.__choose(self.n, k) * self.p ** k * (1 - self.p) ** (self.n - k)
-    
+        return self.__choose(self.n, k) * self.p ** k\
+            * (1 - self.p) ** (self.n - k)
+
     def cdf(self, k):
         """ Calculates the CDF of a given number of successes
         Args:
-            k (float | int): the number of successes whose CDF is to be calculated
+            k (float | int): the number of successes
+            whose CDF is to be calculated
         Returns:
-            p (float): the probability of successes from 0 through k (inclusive).
+            p (float): the probability of successes
+            from 0 through k (inclusive).
         """
         k = int(k)
 
         if k < 0:
             return 0
-        
-        return sum(map(lambda x: self.pmf(x), range(0, k + 1)))
     
+        return sum(map(lambda x: self.pmf(x), range(0, k + 1)))
+
     def __choose(self, n, k):
-        """ Function that calculates the number of k combinations that can be made from n.
+        """ Function that calculates the number of k combinations
+        that can be made from n.
         Args:
-            n (int): the size of the set from which combinations are to be drawn
+            n (int): the size of the set from which combinations 
+            are to be drawn
             k (int): the size of each combination to be drawn from the set
         Returns:
             (int) the number of combinations that can be made
         """
         return self.__fact(n) / self.__fact(k) * self.__fact(n - k)
-    
+
     def __fact(self, n):
         """ Calculates the factorial of a given number n.
         Args:
@@ -99,10 +106,10 @@ class Binomial:
         """
         if not isinstance(n, int):
             raise TypeError('n must be an int')
-        
+
         if n < 0:
             raise ValueError('n must be greater than or equal to zero')
-        
+
         if n == 0 or n == 1:
             return 1
         else:
