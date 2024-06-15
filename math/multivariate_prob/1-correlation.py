3bbd7e1c0fe94af109491a8944487456f9ba7747
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-15 23:10:27 +0200

    1

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -18,7 +18,14 @@ def correlation(C):
 
     if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
+    
+    d = C.shape[0]
 
-    stddevs = np.std(C, axis=0).reshape(1, -1)
+    stddevs = []
+
+    for i in range(d):
+        stddevs.append(C[i, i])
+
+    stddevs = np.array(stddevs).reshape(1, -1)
 
     return C / (stddevs.T @ stddevs)
