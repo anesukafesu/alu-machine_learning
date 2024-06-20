55ea0f4475eb1d960c4909e70cdeaea98f34080f
Anesu Kafesu <a.kafesu@alustudent.com>
2024-06-20 21:50:21 +0200

    0

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -19,8 +19,8 @@ def likelihood(x, n, P):
         raise ValueError('n must be a positive integer')
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError('x must be an integer that \
-                         is greater than or equal to 0')
+        msg = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(msg)
 
     if x > n:
         raise ValueError('x cannot be greater than n')
@@ -29,7 +29,7 @@ def likelihood(x, n, P):
         raise TypeError('P must be a 1D numpy.ndarray')
 
     if not np.all((P >= 0) & (P <= 1)):
-        raise ValueError('All values in P must be in range [0, 1]')
+        raise ValueError('All values in P must be in the range [0, 1]')
 
     return __choose(n, x) * P ** x * (1 - P) ** (n - x)
 
