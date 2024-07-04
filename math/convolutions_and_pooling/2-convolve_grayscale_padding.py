3f862a3c4ddeb4b0707fcb5c6f99e79944d87b3d
Anesu Kafesu <a.kafesu@alustudent.com>
2024-07-04 12:32:19 +0200

    2

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -11,23 +11,26 @@ def convolve_grayscale_padding(images, kernel, padding):
         images (numpy.ndarray) a list of images in the shape (m, h, w)
         kernel (numpy.ndarray) an image in the shape of (kh, kw)
     """
-    # Extract shape from images and kernel
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-
     # Extract padding
-    ph = padding[0] // 2
-    pw = padding[1] // 2
+    ph, pw = padding
 
     # Apply padding
     padded_images = np.pad(images, ((0,), (ph,), (pw,)), mode='constant')
 
+    # Extract shape from padded_images and kernel
+    m, h, w = padded_images.shape
+    kh, kw = kernel.shape
+
+    # Calculate size of images
+    oh = h - kh + 1
+    ow = w - kw + 1
+
     # Creating an array of convolved images
-    convolved_images = np.ones((m, h, w))
+    convolved_images = np.ones((m, oh, ow))
 
     # Loop through images applying kernel to calculate convolutions
-    for i in range(h):
-        for j in range(w):
+    for i in range(oh):
+        for j in range(ow):
             patch = padded_images[:, i: i + kh, j: j + kw]
             convolved_images[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
 
