a3a69d8cc965a97f825bd5660d8ec9ca79a1fd3e
Anesu Kafesu <a.kafesu@alustudent.com>
2024-07-07 04:57:32 +0200

    used zeros

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,6 +9,8 @@ def convolve_grayscale_valid(images, kernel):
     Args:
         images (numpy.ndarray) a list of images in the shape (m, h, w)
         kernel (numpy.ndarray) an image in the shape of (kh, kw)
+    Returns:
+        (numpy.ndarray) the convolved image
     """
     # Extract shape from images and kernel
     m, h, w = images.shape
@@ -19,7 +21,7 @@ def convolve_grayscale_valid(images, kernel):
     ow = w - kw + 1
 
     # Creating an array of convolved images
-    convolved_images = np.ones((m, oh, ow))
+    convolved_images = np.zeros((m, oh, ow))
 
     # Loop through images applying kernel to calculate convolutions
     for i in range(oh):
