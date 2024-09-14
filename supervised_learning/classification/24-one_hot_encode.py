f925fe4791b772c55ab0856558f6dc0ab6a71d44
Anesu Kafesu <a.kafesu@alustudent.com>
2024-09-14 22:08:33 +0200

    24

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Module to implement one_hot_encode
+"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    encoded_Y = []
+    label_lookup = {}
+    labels_so_far = 0
+
+    for label in Y:
+        if not label in label_lookup:
+            n_prefix_zeros = labels_so_far
+            n_suffix_zeros = classes - labels_so_far - 1
+            vector = [0] * n_prefix_zeros + [1] + [0] * n_suffix_zeros
+
+            label_lookup[label] = vector
+            labels_so_far + 1
+
+        vector = label_lookup[label]
+        encoded_Y.append(vector)
+
+    return np.array(encoded_Y)
