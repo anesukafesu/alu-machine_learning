21247da0f2e2bf0ddf29927f78dfbd0798f13e07
Anesu Kafesu <a.kafesu@alustudent.com>
2024-09-14 17:47:37 +0200

    8

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module to implement NeuralNetwork
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class to implement a neural network
+    """
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.normal(0, 1, (nodes, nx))
+        self.b1 = np.zeros((nx, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(0, 1, (2, nodes))
+        self.b1 = 0
+        self.A2 = 0
+        
