e5489d669d0c6ac4529bfcc64bf2ab36c8de2103
Anesu Kafesu <a.kafesu@alustudent.com>
2024-09-14 22:23:24 +0200

    fixes

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
-    encoded_Y = []
-    label_lookup = {}
-    labels_so_far = 0
+    """ Performs one hot encoding
+    Args:
+        Y: the vector with the classes
+        classes: the maximum number of classes
+    Result:
+        numpy.ndarray | None
+    """
+    if not isinstance(Y, np.ndarray):
+        return None
+    if not isinstance(classes, int):
+        return None
 
-    for label in Y:
-        if not label in label_lookup:
-            n_prefix_zeros = labels_so_far
-            n_suffix_zeros = classes - labels_so_far - 1
-            vector = ([0] * n_prefix_zeros) + ([1]) + ([0] * n_suffix_zeros)
-
-            label_lookup[label] = vector
-            labels_so_far += 1
-
-        vector = label_lookup[label]
-        encoded_Y.append(vector)
-
-    return np.array(encoded_Y)
+    try:
+        result = np.eye(classes)[Y].T
+        return result
+    except Exception:
+        return None
