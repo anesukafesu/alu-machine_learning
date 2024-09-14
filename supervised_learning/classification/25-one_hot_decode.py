7135fb73dc4d33a9eed847a92f3cbc7153cc4a9d
Anesu Kafesu <a.kafesu@alustudent.com>
2024-09-14 22:29:27 +0200

    15

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Module to implement one_hot_encode
+"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """ Performs one hot decoding
+    Args:
+        one_hot: a one hot encoded matrix
+    Result:
+        numpy.ndarray | None
+    """
+    if not isinstance(one_hot, np.ndarray):
+        return None
+
+    if  one_hot.ndim != 2:
+        return None
+
+    return one_hot.T.argmax(axis=1)
