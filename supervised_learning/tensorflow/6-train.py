7c327a56e5e361199b1fa619d7251319cd9192a4
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-09-21 21:27:51 +0000

    6-fix

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -57,14 +57,10 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
         sess.run(init)
 
         for i in range(iterations):
-        loss_train = sess.run(loss,
-                              feed_dict={x: X_train, y: Y_train})
-        accuracy_train = sess.run(accuracy,
-                                  feed_dict={x: X_train, y: Y_train})
-        loss_valid = sess.run(loss,
-                              feed_dict={x: X_valid, y: Y_valid})
-        accuracy_valid = sess.run(accuracy,
-                                  feed_dict={x: X_valid, y: Y_valid})
+            loss_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+            accuracy_train = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+            loss_valid = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            accuracy_valid = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
 
             if i % 100 == 0:
                 print("After {} iterations:".format(i))
