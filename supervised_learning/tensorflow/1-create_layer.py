1c73f801e67066220d403b1c9e92103153078344
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-09-21 20:21:44 +0000

    pycodestyle

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -15,5 +15,5 @@ def create_layer(prev, n, activation):
     """
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(units=n, activation=activation,
-                        kernel_initializer=init, name='layer')
+                            kernel_initializer=init, name='layer')
     return layer(prev)
