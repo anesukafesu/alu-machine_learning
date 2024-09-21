a3ebf7fa4b74a0d291cb93b873b8280d8143e76b
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-09-21 21:19:46 +0000

    7-fix

---
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Implements evaluate
+"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """ Evaluates output of neural network
+    Args:
+        X (numpy.ndarray) contains the input data to evaluate
+        Y (numpy.ndarray) contains the one-hot labels for X
+        save_path (string) location to load the model from
+    Returns:
+        (tuple) prediction, accuracy, loss
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
+    return (prediction, accuracy, loss)
