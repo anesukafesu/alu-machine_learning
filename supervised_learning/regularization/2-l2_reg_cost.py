00011e1f22299b9755a4a1dd093120c2b309f4e1
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-09-27 15:09:36 +0000

    2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -14,8 +14,4 @@ def l2_reg_cost(cost):
     tensorflow.Tensor: a tensor representing the cost of the
     network with the l2 regularisation term.
     """
-    weights = tf.trainable_weights()
-
-    l2_term = tf.add_n([tf.reduce_sum(tf.square(w)) for w in weights])
-
-    return cost + l2_term
+    return cost + tf.losses.get_regularization_losses()
