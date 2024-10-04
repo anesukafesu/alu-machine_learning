f60d7b6c9e66825860da417fadc992d8cbb6a3b0
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-10-04 21:30:52 +0000

    0

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+""" Implements create_confusion_matrix
+"""
+
+
+def create_confusion_matrix(labels, logits):
+    """ Creates a confusion matrix using labels and logits
+    Args:
+        labels (numpy.ndarray): A one-hot encoded numpy.ndarray
+        containing the correct labels for each data point. The
+        array is in the shape (m, classes) where m is the number
+        of datapoints and classes is the number of classes in the
+        dataset.
+        
+        logits (numpy.ndarray): A one-hot enocded numpy.ndarray
+        containing the predictions made by the model for each
+        data point. The array is in the shape (m, classes) where
+        m is the number of data points and classes is the number
+        of classes in the dataset.
+
+    Returns:
+        (numpy.ndarray) The confusion matrix of the model with
+        the shape (classes, classes).
+    """
+    return labels.T @ logits
