4f1394d66a2cdbc0bde7869d9fff3cf7716f0d67
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2024-10-05 19:46:37 +0000

    1

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Implements create_confusion_matrix
+"""
+import numpy as np
+
+
+def calculate_sensitivity(confusion_matrix):
+    """ Calculate the sensitivity of each class in a given 
+    confusion matrix.
+
+    Args:
+        confusion_matrix (numpy.ndarray) The confusion matrix
+
+    Returns:
+        numpy.ndarray: The sensitivity of each class
+    """
+    true_positives = np.diag(confusion_matrix)
+    true_positives_plus_false_negatives = np.sum(axis=0)
+    return true_positives / true_positives_plus_false_negatives
