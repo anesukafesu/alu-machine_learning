ef2840176c35dce3f8175c2360d7244c47839f2b
Anesu Kafesu <a.kafesu@alustudent.com>
2024-10-25 19:16:05 +0200

    2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -6,15 +6,16 @@ import sys
 import requests
 import json
 
-url = sys.argv[1]
-response = requests.get(url)
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
 
-if response.status_code == 200:
-    data = json.loads(response.text)
-    print(data['location'])
-elif response.status_code == 403:
-    print('Reset in ')
-elif response.status_code == 404:
-    print('Not found')
-else:
-    print('Error:', response.status_code)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        print(data['location'])
+    elif response.status_code == 403:
+        print('Reset in ')
+    elif response.status_code == 404:
+        print('Not found')
+    else:
+        print('Error:', response.status_code)
