d270fec6034f138f7029d9e4112299af520f668f
Anesu Kafesu <a.kafesu@alustudent.com>
2024-11-09 12:23:14 +0200

    data augmentation tasks 0-5

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -6,14 +6,14 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_coll = client.logs.nginx
-    doc_count = logs_coll.count_documents({})
+    logs = client.logs.nginx
+    doc_count = logs.count_documents({})
     print("{} logs".format(doc_count))
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        method_count = logs_coll.count_documents({"method": method})
+        method_count = logs.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, method_count))
     filter_path = {"method": "GET", "path": "/status"}
-    path_count = logs_coll.count_documents(filter_path)
+    path_count = logs.count_documents(filter_path)
     print("{} status check".format(path_count))
